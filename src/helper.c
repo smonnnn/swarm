a01ed63033e849b86a4fc5a05f2f8ce1f87233d8
smonnnn <slooij1@gmail.com>
2026-01-27 10:16:34 +0100

    fix

--- src/helper.c
+++ src/helper.c
@@ -1,5 +1,5 @@
 #include "helper.h"
-#define STAGING_BUFFER_SIZE (32 * 1024 * 1024)   //32MB buffer for copying data onto the GPU.
+#define STAGING_BUFFER_SIZE (512 * 1024 * 1024)   //32MB buffer for copying data onto the GPU.
 
 VKCTX* get_ctx(){
     static VKCTX ctx = {0};
@@ -31,11 +31,13 @@ VKBUFFER to_gpu(void* data, size_t size){
     VKBUFFER* staging = get_staging_buffer();
     VKBUFFER buffer = newBuffer(*ctx, size, BUF_GPU);
     void* mapped;
+    int segment_size;
     for(int i = 0; i < size; i += STAGING_BUFFER_SIZE){
+        segment_size = min(STAGING_BUFFER_SIZE, (size - i));
         mapped = mapBuffer(*ctx, *staging);
-        memcpy(mapped, data, size);
+        memcpy(mapped, data, segment_size);
         unmapBuffer(*ctx, *staging);
-        copyBufferSlow(*ctx, mapped, buffer, i, i, min(STAGING_BUFFER_SIZE, (size - i)));
+        copyBufferSlow(*ctx, mapped, buffer, i, i, segment_size);
     }
     return buffer;
 }
